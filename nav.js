0b42b7f2f4d64eb56f5dafd276c29c0bcdb00a70
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-11-21 01:11:48 +0000

    really basic js nav menu item cms

---
+++ nav.js
@@ -0,0 +1,5 @@
+let menu1 = document.getElementById("1").innerText= "Services";
+let menu2 = document.getElementById("2").innerText= "Work";
+let menu3 = document.getElementById("3").innerText= "Clients";
+let menu4 = document.getElementById("4").innerText= "About";
+let menu5 = document.getElementById("5").innerText= "News";
