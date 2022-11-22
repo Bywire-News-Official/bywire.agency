160e8fe5d3dd42f76f570993c8f1f09772b4bc0a
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-11-22 15:34:48 +0000

    new mobile menu

--- nav.js
+++ nav.js
@@ -3,3 +3,14 @@ let menu2 = document.getElementById("2").innerText= "Work";
 let menu3 = document.getElementById("3").innerText= "Clients";
 let menu4 = document.getElementById("4").innerText= "About";
 let menu5 = document.getElementById("5").innerText= "News";
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
