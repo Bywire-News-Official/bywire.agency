8bbfed9f4f89f282a29667f5f0d2afb0f1b82b0d
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-12-03 01:16:20 +0000

    high res logo + js

--- nav.js
+++ nav.js
@@ -1,8 +1,3 @@
-let menu1 = document.getElementById("1").innerText= "Services";
-let menu2 = document.getElementById("2").innerText= "Work";
-let menu3 = document.getElementById("3").innerText= "Clients";
-let menu4 = document.getElementById("4").innerText= "About";
-let menu5 = document.getElementById("5").innerText= "News";
 
 
 /* Open when someone clicks on the span element */
